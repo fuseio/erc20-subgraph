d06c40e8adfcfda8a66ef5f0f1be8a7065324fa6
Lior Agnin <lior.agnin@gmail.com>
2023-07-04 11:24:43 +0300

    Handle WFUSE events

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,3 +1,4 @@
+import { Deposit, Withdrawal } from './../generated/WETH9/WETH9';
 //import event class from generated files
 import { Transfer } from "../generated/ERC20/ERC20"
 //import entities
@@ -58,5 +59,70 @@ export function handleTransfer(event: Transfer): void {
   toTokenBalance.amount = fetchBalance(event.address, event.params.to)
   if (toTokenBalance.amount != BigDecimal.fromString("0")) {
     toTokenBalance.save();
+  }
+}
+
+export function handleDeposit(event: Withdrawal): void {
+  let token = fetchTokenDetails(event);
+  if (!token) { //if token == null
+    return
+  }
+
+  // get account addresses from event
+  let toAddress = event.params.src.toHex();
+
+  // fetch account details
+  let toAccount = fetchAccount(toAddress);
+
+  if (!toAccount) {
+    return;
+  }
+
+  // setting the token balance of the 'to' account
+  let toTokenBalance = TokenBalance.load(token.id + "-" + toAccount.id);
+  if (!toTokenBalance) {
+    toTokenBalance = new TokenBalance(token.id + "-" + toAccount.id);
+    toTokenBalance.token = token.id;
+    toTokenBalance.account = toAccount.id;
+  }
+  toTokenBalance.amount = fetchBalance(event.address, event.params.src)
+  if (toTokenBalance.amount != BigDecimal.fromString("0")) {
+    toTokenBalance.save();
+  }
+}
+
+export function handleWithdrawal(event: Deposit): void {
+  let token = fetchTokenDetails(event);
+  if (!token) { //if token == null
+    return
+  }
+
+  // get account addresses from event
+  let fromAddress = event.params.dst.toHex();
+
+  // fetch account details
+  let fromAccount = fetchAccount(fromAddress);
+
+  if (!fromAccount) {
+    return;
+  }
+
+  // setting the token balance of the 'from' account
+  let fromTokenBalance = TokenBalance.load(token.id + "-" + fromAccount.id);
+  if (!fromTokenBalance) { //if balance is not already saved
+    // create a new TokenBalance instance
+    // while creating the new token balance,
+    // the combination of the token address 
+    // and the account address is  
+    // passed as the identifier value
+    fromTokenBalance = new TokenBalance(`${token.id}-${fromAccount.id}`);
+    fromTokenBalance.token = token.id;
+    fromTokenBalance.account = fromAccount.id;
+  }
+
+  fromTokenBalance.amount = fetchBalance(event.address, event.params.dst)
+  // filtering out zero-balance tokens - optional
+  if (fromTokenBalance.amount != BigDecimal.fromString("0")) {
+    fromTokenBalance.save();
   }
 }
